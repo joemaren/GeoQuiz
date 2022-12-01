c98fc5c937c495b552a9e282d2ba4eb089301669
Joseph Kurape <joemaren@users.noreply.github.com>
2022-12-01 23:19:25 +0100

    Wire the buttons to listeners

--- app/src/main/java/ng/joe/android/geoquiz/MainActivity.kt
+++ app/src/main/java/ng/joe/android/geoquiz/MainActivity.kt
@@ -2,10 +2,24 @@ package ng.joe.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            // Do something in response to the click here)
+        }
+        falseButton.setOnClickListener {view: View ->
+            // Do something in response to the click here
+        }
     }
 }
